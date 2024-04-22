346ae56cbf888bf443935f3e42cd93668164fb27
austinbrot <abrotman@caltech.edu>
2024-04-22 14:32:26 -0700

    update mem and logging

--- scripts/caiman/preproc_caiman_wrapper.sh
+++ scripts/caiman/preproc_caiman_wrapper.sh
@@ -3,15 +3,15 @@
 #SBATCH --time=04:00:00                     # run job for max 4 hours
 #SBATCH --nodes=1                           # number of nodes (1 node = 1 computer)
 #SBATCH --ntasks=16                         # number of processor cores (i.e. tasks)
-#SBATCH --mem-per-cpu=6G                    # memory per CPU core
+#SBATCH --mem-per-cpu=8G                    # memory per CPU core
 
 #SBATCH -J "caiman"                         # job name
 #SBATCH --mail-user=abrotman@caltech.edu    # email address
 #SBATCH --mail-type=END
 #SBATCH --mail-type=FAIL
 
-#SBATCH -o /central/home/$USER/slurmout/slurm.%N.%j.out # STDOUT
-#SBATCH -e /central/home/$USER/slurmout/slurm.%N.%j.err # STDERR
+##SBATCH -o ~/slurmout/slurm.%N.%j.out # STDOUT
+##SBATCH -e ~/slurmout/slurm.%N.%j.err # STDERR
 
 # If you require a simple way to access an entire nodes resources
 # including memory, use the exclusive switch. This could possibly
