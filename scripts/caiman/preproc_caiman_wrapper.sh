e9f95b44aaaa4b886f81fa70ccedcd4bcfe577be
austinbrot <abrotman@caltech.edu>
2024-04-29 14:36:42 -0700

    increase memory

--- scripts/caiman/preproc_caiman_wrapper.sh
+++ scripts/caiman/preproc_caiman_wrapper.sh
@@ -4,7 +4,7 @@
 #SBATCH --nodes=1                           # number of nodes (1 node = 1 computer)
 #SBATCH --ntasks=1                          # number of processor cores (i.e. tasks)
 #SBATCH --cpus-per-task=32                   # number of OpenMP threads
-#SBATCH --mem-per-cpu=6G                    # memory per CPU core
+#SBATCH --mem-per-cpu=12G                    # memory per CPU core
 
 #SBATCH -J "caiman"                         # job name
 #SBATCH --mail-user=abrotman@caltech.edu    # email address
